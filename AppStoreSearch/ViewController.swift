19a1bbae710e9797c11ffa6b0f3b24d3666b2ae4
옥인준 <injunock@naver.com>
2022-08-08 17:58:37 +0900

    [+] searchBar

--- AppStoreSearch/ViewController.swift
+++ AppStoreSearch/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "검색어를 입력하세요."
+        searchBar.searchBarStyle = .prominent
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.searchTextField.layer.borderWidth = 1
+        searchBar.searchTextField.clipsToBounds = true
+        searchBar.searchTextField.layer.cornerRadius = 16
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setNavigationController()
+        setUI()
+        setConstraints()
     }
-
-
+    
+    // navigation controller setting
+    private func setNavigationController() {
+        navigationItem.title = "검색"
+        view.backgroundColor = .white
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .clear
+    }
+    
+    private func setUI() {
+        view.addSubview(searchBar)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 }
 
