ca8fea404f635759c065f2dd02086ac3bea9cda5
옥인준 <injunock@naver.com>
2022-08-09 15:43:31 +0900

    [+] Imagecache, ImageUtil

--- AppStoreSearch/Extenstion/UIImageViewExtension.swift
+++ AppStoreSearch/Extenstion/UIImageViewExtension.swift
@@ -5,4 +5,14 @@
 //  Created by injun on 2022/08/09.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImage(url: URL) {
+        ImageCache.shared.caching(url: url) { [weak self] uiImage in
+            DispatchQueue.main.async {
+                self?.image = uiImage
+            }
+        }
+    }
+}
--- AppStoreSearch/Util/ImageCache.swift
+++ AppStoreSearch/Util/ImageCache.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+
+class ImageCache {
+    static let shared = ImageCache()
+    
+    private let cache = NSCache<NSString, UIImage>()
+    
+    func caching(url: URL, block: @escaping (UIImage?) -> Void) {
+        let key = NSString(string: url.absoluteString)
+        if let image = cache.object(forKey: key) {
+            block(image)
+        }
+        
+        requestImage(url: url) { [weak self] data in
+            guard let data = data else {
+                block(nil)
+                return
+            }
+
+            if let image = UIImage(data: data) {
+                self?.cache.setObject(image, forKey: key)
+                block(image)
+            } else {
+                block(nil)
+            }
+        }
+    }
+    
+    private func requestImage(url: URL, block: @escaping (Data?) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            URLSession.shared.dataTask(with: url) {data, response, error in
+                block(data)
+            }.resume()
+        }
+    }
+}
